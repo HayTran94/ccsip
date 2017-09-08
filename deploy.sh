345a572366ba28a7c58f3076a6e2f4908a00d2a2
github1 <john.collier4@gmail.com>
2017-09-08 00:38:46 -0400

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -f .work/dropler/deploy.sh ]; then
+  mkdir -p tmp
+  git clone https://github.com/github1/dropler.git .work/dropler
+fi
+
+sh .work/dropler/deploy.sh "${@:1}"
