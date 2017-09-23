e78758f670da5d0ac4f52635890c11c9a0ed52c0
github1 <john.collier4@gmail.com>
2017-09-23 00:15:39 -0400

    auto refresh dispatcher.list

--- kamailio/run.sh
+++ kamailio/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+refreshDispatcher() {
+  until pids=$(pidof kamailio)
+  do
+    sleep 1
+  done
+  sleep 5
+  touch /etc/kamailio/dispatcher.list
+}
+
 consul-template -consul-addr consul:8500 -template '/tmp/dispatcher.list.tpl:/etc/kamailio/dispatcher.list' &
+
+refreshDispatcher &
 
 /run.sh
