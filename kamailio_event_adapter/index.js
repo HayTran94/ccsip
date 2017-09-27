7af156a4db7dd227c3cd85de8883d93a0a00eb8a
github1 <john.collier4@gmail.com>
2017-09-27 01:53:19 -0400

    emit events from kamailio

---
+++ kamailio_event_adapter/index.js
@@ -0,0 +1,19 @@
+const meshage = require('meshage');
+const clusterPort = 9991;
+
+const staticNodes = [{
+    id: `kamailio-event-adapter`,
+    self: true,
+    host: process.env.PRIVATE_ADDR,
+    port: clusterPort
+}];
+
+new meshage.MessageRouter(
+    9992,
+    new meshage.GossiperCluster(clusterPort, new meshage.StaticPeerProvider(staticNodes))
+).start((err, router) => {
+    router.register('kamailio-events', (command) => {
+        console.log(command);
+        return { echo: { command } };
+    });
+});
