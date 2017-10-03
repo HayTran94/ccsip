8df086a424f3f75469b432da4c307f2de4132c9c
github1 <john.collier4@gmail.com>
2017-10-02 23:21:29 -0400

    add terraform files

---
+++ terraform/variable.tf
@@ -0,0 +1,14 @@
+variable "ssh_key_id" {
+  description = "Digital Ocean ssh key id"
+  default     = ""
+}
+
+variable "ssh_key_private_path" {
+  description = "Digital Ocean ssh private key path"
+  default     = ""
+}
+
+variable "deployment_tag" {
+  description = "Digital Ocean droplet tag"
+  default     = ""
+}
