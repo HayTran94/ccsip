78870ef557667f14931b723fbaf03db6eb06dac7
github1 <john.collier4@gmail.com>
2017-10-21 06:46:36 -0400

    fix consul port

--- api/run.sh
+++ api/run.sh
@@ -2,7 +2,7 @@
 
 registerConsulService() {
   while true; do
-    if nc -zvw 1 ${CONSUL_HOST} 8510 > /dev/null 2>&1; then
+    if nc -zvw 1 ${CONSUL_HOST} 8500 > /dev/null 2>&1; then
       curl -s -X PUT \
          -d '{"Datacenter": "dc1", "Node": "'"${INSTANCE_NAME}"'", "Address": "'"${PRIVATE_ADDR}"'", "Service": {"Service": "'"${INSTANCE_TYPE}"'", "Port": 5060, "Tags": [ "udp" ]}}' \
          "http://${CONSUL_HOST}:8500/v1/catalog/register" > /dev/null
